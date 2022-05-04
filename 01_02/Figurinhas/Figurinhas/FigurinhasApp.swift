4d25ee2f5b84e937637fe232f17bc10fe68fc44e
Lucas Longo <llongo@linkedin.com>
2022-05-04 06:50:46 -0700

    01_02

---
+++ 01_02/Figurinhas/Figurinhas/FigurinhasApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigurinhasApp.swift
+//  Figurinhas
+//
+//  Created by Lucas Longo on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct FigurinhasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
