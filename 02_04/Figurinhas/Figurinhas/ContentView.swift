feb5e8e18e593a02907158d2454bfed7cb71a46b
Lucas Longo <llongo@linkedin.com>
2022-05-04 07:39:46 -0700

    02_04

---
+++ 02_04/Figurinhas/Figurinhas/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  Figurinhas
+//
+//  Created by Lucas Longo on 3/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        VStack(alignment: .center) {
+            
+            Text("Mingle")
+                .font(.title)
+                .fontWeight(.black)
+                .foregroundColor(Color.gray)
+                .bold()
+                .textCase(/*@START_MENU_TOKEN@*/.lowercase/*@END_MENU_TOKEN@*/)
+            
+            HStack {
+                
+                Text("Frase: ")
+                    .bold()
+                
+                Text("Problema em dobro")
+            }
+            
+            HStack {
+                
+                Image("figura_01")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100)
+                
+                VStack {
+                    
+                    HStack {
+                        Image(systemName: "lifepreserver")
+                            .foregroundColor(Color.green)
+                            .font(.system(size: 30))
+
+                        Text("Vidas: ")
+                        
+                        Text("2")
+                    }
+
+                    HStack {
+                        Image(systemName: "bolt")
+                            .foregroundColor(Color.yellow)
+                            .font(.system(size: 30))
+
+                        Text("Potência: ")
+                        
+                        Text("60%")
+                    }
+                }
+
+            }
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
