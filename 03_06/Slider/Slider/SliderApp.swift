f700367ac40c54ae8188c70f3a2f4f67fccf707e
Lucas Longo <llongo@linkedin.com>
2022-05-04 07:45:43 -0700

    03_06

---
+++ 03_06/Slider/Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Lucas Longo on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
