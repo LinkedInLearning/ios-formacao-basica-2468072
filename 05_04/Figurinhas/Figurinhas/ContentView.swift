5494b62949b733b0ce2073751ad768eb96817c18
Lucas Longo <llongo@linkedin.com>
2022-05-04 12:13:01 -0700

    a

---
+++ 05_04/Figurinhas/Figurinhas/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Figurinhas
+//
+//  Created by Lucas Longo on 3/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        TabView {
+            NavigationView {
+                FigurasView()
+            }
+            .tabItem {
+                Label("Figuras", systemImage: "face.smiling")
+            }
+            FiguraDetalheView(figura: figuras[0])
+                .tabItem {
+                    Label("Minha Coleção", systemImage: "face.dashed")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
